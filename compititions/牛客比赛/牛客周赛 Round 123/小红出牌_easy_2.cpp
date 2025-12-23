1065024d71999710c5ae1cfcf0447b8f5cc8e3c4
Beau-Will <1042383080@qq.com>
2025-12-24 02:50:12 +0800

    牛客周赛 Round 123 E and F problems better solution

---
+++ compititions/牛客比赛/牛客周赛 Round 123/小红出牌_easy_2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+
+constexpr int inf1 = 1E9,inf2 = 0x3f3f3f3f;
+constexpr int P1 = 1E9+7, P2 = 998'244'353;
+
+void solve() {
+  int n;
+  std::cin >> n;
+
+  std::vector<int> a(n);
+  for(int i = 0; i < n; i++){
+    std::cin >> a[i];
+  }
+
+  std::vector<bool> vis(n+1);
+  int ans = 0;
+  for(int i = 0; i < n; i++){
+    vis[a[i]] = true;
+    if(vis[a[i]+1] && vis[a[i]-1]){
+      ans--;
+    }else if(!vis[a[i]+1] && !vis[a[i]-1]){
+      ans++;
+    }
+    std::cout << ans << " \n"[i+1==n];
+  }
+}
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  int T = 1;
+  // std::cin >> T;
+  for (; T--;) {
+    solve();
+  }
+
+  return 0;
+}
