6b2feec6cef1a92da366f61fbb93aa47a2840dda
Beau-Will <1042383080@qq.com>
2025-12-29 18:32:03 +0800

    251229 Daily_CF_Problems one problem solved

---
+++ problems/Daily_CF_Problems/251229/C_John_and_Olaf.cpp
@@ -0,0 +1,164 @@
+/*
+2025.12.29 Daily Two Problems
+Problem：https://codeforces.com/gym/104786/problem/C
+Editorial：https://github.com/Yawn-Sean/Daily_CF_Problems/blob/main/daily_problems/2025/12/1229/solution/cf104786c.md
+*/
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+constexpr int inf1 = 1E9;
+constexpr int P1 = 1E9+7, P2 = 998'244'353;
+constexpr i64 inf2 = 1E18;
+
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+ 
+template<int P>
+struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(i64 x) : x{norm(x % getMod())} {}
+     
+    static int Mod;
+    constexpr static int getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(int Mod_) {
+        Mod = Mod_;
+    }
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr int val() const {
+        return x;
+    }
+    explicit constexpr operator int() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        x = 1LL * x * rhs.x % getMod();
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        i64 v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+ 
+template<>
+int MInt<0>::Mod = 998244353;
+ 
+template<int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+ 
+constexpr int P = 1000000007;
+using Z = MInt<P>;
+
+void solve(){
+  int n,x,y;
+  std::cin >> n >> x >> y;
+
+  //x表示x到n的距离，y表示y到n的距离
+  x = n-x;
+  y = n-y;
+
+  std::vector<std::vector<Z>> dp(x+1,std::vector<Z>(x+1,0));
+
+  for(int i = 0; i <= x; i++){
+    for(int j = 0; j < i; j++){
+      //Johm往右走，碰到N则往左走
+      Z v = dp[i-1][std::abs(j-1)];
+      //如果John往左走不会相遇
+      if(i-1 >= j+1){
+        v += dp[i-1][j+1];
+      }
+      dp[i][j] = v*Z(2).inv()+1;
+    }
+  }
+
+  std::cout << dp[x][y] << "\n";
+}
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int T = 1;
+  // std::cin >> T;
+  for(; T--;){
+    solve();
+  }
+
+  return 0;
+}
