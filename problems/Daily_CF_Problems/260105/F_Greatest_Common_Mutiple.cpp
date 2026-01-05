60e408d6f1e1e98b71a384766c875b70e6cddae9
Beau-Will <1042383080@qq.com>
2026-01-05 22:57:54 +0800

    260105 Daily_CF_Problems two problems solved

---
+++ problems/Daily_CF_Problems/260105/F_Greatest_Common_Mutiple.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+constexpr int inf1 = 1E9;
+constexpr int P1 = 1E9+7, P2 = 998'244'353;
+constexpr i64 inf2 = 1E18;
+
+void solve(){
+  i64 a,b,c;
+  std::cin >> a >> b >> c;
+
+  i64 lcm = a/std::gcd(a,b)*b;
+
+  std::cout << c-std::gcd(lcm,c) << "\n";
+}
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int T = 1;
+  std::cin >> T;
+  for(; T--;){
+    solve();
+  }
+
+  return 0;
+}
