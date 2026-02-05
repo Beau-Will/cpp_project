33884614e5b241f29f7c1b417000a38baa0bc409
Beau-Will <1042383080@qq.com>
2026-02-06 00:05:41 +0800

    260206牛客tracker每日一题

---
+++ problems/牛客/tracker每日一题/2026/02/260206_【模板】巴什博弈.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void solve(){
+  int n, m;
+  std::cin >> n >> m;
+
+  if(n % (m + 1)){
+    std::cout << "YES\n";
+  }else{
+    std::cout << "NO\n";
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int T = 1;
+  std::cin >> T;
+
+  for(int Ti = 0; Ti < T; Ti++){
+    solve();
+  }
+
+  return 0;
+}
